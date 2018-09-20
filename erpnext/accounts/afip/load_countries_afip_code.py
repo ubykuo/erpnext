fa56878482bd8e4a37a2ecd330f221f7bcd067c6
juancruzgardey <juancruzgardey@hotmail.com>
2018-09-20 19:46:09 -0300

    fixed Translate function when the text to translate is not found

--- erpnext/accounts/afip/load_countries_afip_code.py
+++ erpnext/accounts/afip/load_countries_afip_code.py
@@ -8,17 +8,15 @@ def Translate(source, target, text):
     cabeceras = {"Charset":"UTF-8","User-Agent":"AndroidTranslate/5.3.0.RC02.130475354-53000263 5.1 phone TRANSLATE_OPM5_TEST_1"}
     url = "https://translate.google.com/translate_a/single?client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&hl=es-ES&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&iid=1dd3b944-fa62-4b55-b330-74909a99969e"
     response = requests.post(url, data=parametros, headers=cabeceras)
-    if response.status_code == 200:
+    if response.status_code == 200 and response.json().get("sentences", None):
         for x in response.json()['sentences']:
-            return x['trans']
+            return x.get('trans', None)
     else:
-        return "Ocurrio un error"
-
+        return None
 
 def update_countries():
     service = connect_afip("wsfex")
     countries = service.GetParamDstPais()
-
     for country in countries:
         country = country.split("|")
         country_translated = Translate("es", "en", country[2])
