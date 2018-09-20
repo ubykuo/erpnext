e88b469f04c87f6ec133c459478bb8888ca02ad4
juancruzgardey <juancruzgardey@hotmail.com>
2018-09-20 19:43:51 -0300

    script to assign the code of the languages ​​supported by AFIP, to the languages ​​of erpnext

---
+++ erpnext/accounts/afip/load_languages_afip_code.py
@@ -0,0 +1,24 @@
+from load_countries_afip_code import Translate
+from erpnext.accounts.afip.afip import connect_afip
+import frappe
+
+
+def load_language_codes():
+    # convert afip languages to {name, code} dictionary
+    afip_languages = list(map(lambda l: {"name": l.split("|")[2], "code": l.split("|")[1]}, connect_afip("wsfex").GetParamIdiomas()))
+
+    for language in frappe.get_all("Language", fields=["*"]):
+        language_in_spanish = Translate(language["language_code"], "es", language["language_name"])
+        afip_language = get_afip_language(language_in_spanish, afip_languages)
+        if afip_language:
+            language_object = frappe.get_doc("Language", language["language_code"])
+            language_object.afip_code = afip_language["code"]
+            language_object.save()
+
+
+def get_afip_language(language, afip_languages):
+    if not language:
+        return None
+    found = filter(lambda l: l["name"].upper() in language.upper(), afip_languages)
+    return found[0] if found else None
+
